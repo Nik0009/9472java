b484e69fd58cc7f6651caf85ca425a13757c254d
Nikhil Mowade <91021757+Nik0009@users.noreply.github.com>
2021-12-09 18:49:32 +0530

    Update and rename Main.java to Exercise1.java

--- Java_praticals/w3resourceProblems/Java Conditional Statement Exercises/Problem no1/Exercise1.java
+++ Java_praticals/w3resourceProblems/Java Conditional Statement Exercises/Problem no1/Exercise1.java
@@ -8,7 +8,7 @@ Number is positive
 
 */
 import java.util.Scanner;
-public class Main {
+public class Exercise1 {
   public static void main(String[] args) {
   // Write your code here
   Scanner obj=new Scanner(System.in);
