76bb8595a0c2f6ce163e09110678a745eca94061
Nikhil Mowade <91021757+Nik0009@users.noreply.github.com>
2021-12-09 18:28:10 +0530

    Delete ArrayExample.java

--- Java_Praticals/Question no 11/ArrayExample.java
+++
@@ -1,9 +0,0 @@
-public class ArrayExample{
-public static void main(String args[]){
-double[] students={56.0,5.8,5.3,3,45,10.0};
-
-for(int i=0;i<students.length;i++){
-System.out.println("students="+students[i]);
-}
-}
-}
