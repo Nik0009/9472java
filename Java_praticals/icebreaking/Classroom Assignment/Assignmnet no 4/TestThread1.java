95d8b0fdae439db472d8057a8c6ca2a9f8c13ae0
Nikhil Mowade <91021757+Nik0009@users.noreply.github.com>
2021-12-30 17:21:15 +0530

    Add files via upload

---
+++ Java_praticals/icebreaking/Classroom Assignment/Assignmnet no 4/TestThread1.java
@@ -0,0 +1,83 @@
+/* Assignment no 4*/
+import java.util.Random;  
+import java.util.concurrent.ArrayBlockingQueue;  
+import java.util.concurrent.BlockingQueue;  
+  
+public class TestThread1 {  
+  
+   public static void main(final String[] arguments) throws InterruptedException {  
+      BlockingQueue<Integer> queue = new ArrayBlockingQueue<Integer>(10);  
+  
+      Insert i = new Insert(queue);  
+      Retrieve r = new Retrieve(queue);  
+  
+      new Thread(i).start();  
+      new Thread(r).start();  
+  
+      Thread.sleep(2000);  
+   }    
+  
+  
+   static class Insert implements Runnable {  
+      private BlockingQueue<Integer> queue;  
+  
+      public Insert(BlockingQueue queue) {  
+         this.queue = queue;  
+      }  
+  
+      @Override  
+      public void run() {  
+         Random random = new Random();  
+  
+         try {  
+            int result = random.nextInt(200);  
+            Thread.sleep(1000);  
+            queue.put(result);  
+            System.out.println("Added: " + result);  
+              
+            result = random.nextInt(10);  
+            Thread.sleep(1000);  
+            queue.put(result);  
+            System.out.println("Added: " + result);  
+              
+            result = random.nextInt(50);  
+            Thread.sleep(1000);  
+            queue.put(result);  
+            System.out.println("Added: " + result);  
+         } catch (InterruptedException e) {  
+            e.printStackTrace();  
+         }  
+      }      
+   }  
+  
+   static class Retrieve implements Runnable {  
+      private BlockingQueue<Integer> queue;  
+  
+      public Retrieve(BlockingQueue queue) {  
+         this.queue = queue;  
+      }  
+        
+      @Override  
+      public void run() {  
+           
+         try {  
+            System.out.println("Removed: " + queue.take());  
+            System.out.println("Removed: " + queue.take());  
+            System.out.println("Removed: " + queue.take());  
+         } catch (InterruptedException e) {  
+            e.printStackTrace();  
+         }  
+      }  
+   }  
+}
+/*OUTPUT:
+C:\java programs\Java_Praticals\icebreaking\Classroom Assignment\Assignmnet no 4>javac TestThread1.java
+
+C:\java programs\Java_Praticals\icebreaking\Classroom Assignment\Assignmnet no 4>java TestThread1
+Added: 183
+Removed: 183
+Added: 1
+Removed: 1
+Added: 40
+Removed: 40
+*/
