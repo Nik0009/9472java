43ba01df1a434de390af257e57f25873bcb82162
Nikhil Mowade <91021757+Nik0009@users.noreply.github.com>
2022-01-07 16:12:30 +0530

    Update AppTest.java

--- Java_praticals/icebreaking/07.01.2022/AppTest.java
+++ Java_praticals/icebreaking/07.01.2022/AppTest.java
@@ -1,14 +1,37 @@
-package nikhil;
+package com.anudip.nikhil;
 
-import static org.junit.Assert.*;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
-import org.junit.Test;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
 
-public class AppTest {
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
-}
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
