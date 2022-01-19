1c71d91f8d6e04910be097250e320db96682cbbf
Nikhil Mowade <91021757+Nik0009@users.noreply.github.com>
2022-01-19 21:11:56 +0530

    Add files via upload

---
+++ Java_praticals/icebreaking/15.12.2022/CompileTimePolymorphismExample.java
@@ -0,0 +1,37 @@
+/*     COMIPLETIME POLYMORPHISM EXAMPLE    */
+class ClassMain{
+
+void dis(int number){
+System.out.println("method:"+number);
+}
+void dis(int number1,int number2){
+System.out.println("method:"+number1+","+number2);
+}
+double dis(double number){
+System.out.println("method:"+number);
+return number;
+}
+}
+
+class CompileTimePolymorphismExample{
+public static void main(String args[]){
+ClassMain obj=new ClassMain();
+double result;
+obj.dis(40);
+obj.dis(70,30);
+result=obj.dis(6.1);
+System.out.println("Answer is:"+result);
+}
+}
+
+/* 
+o/p-
+C:\java programs\Java_Praticals\icebreaking\15.12.2021>javac CompileTimePolymorphismExample.java
+
+C:\java programs\Java_Praticals\icebreaking\15.12.2021>java CompileTimePolymorphismExample
+method:40
+method:70,30
+method:6.1
+Answer is:6.1
+
+*/
